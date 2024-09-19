8eb62189d19f00b35f6d05b42b6d7cb9a5933d18
Marcos Vinicius <viniciusmarcos136@gmail.com>
2024-09-19 17:00:23 -0300

    att

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,9 @@
 import quickbill from "./quickbill/utils.js";
 import escFunction from "./secondaries-scripts/backpage.js";
+import { abc } from "./secondaries-scripts/toggleScreen.js";
 
 quickbill()
 escFunction()
+abc()
 
 
---
+++ javascript/secondaries-scripts/toggleScreen.js
@@ -0,0 +1,7 @@
+const buttonQuickmeter = document.querySelector(".buttonQuickmeter")
+
+export function abc(){
+ buttonQuickmeter.addEventListener("click", () => {
+    alert("oi")
+})
+}
